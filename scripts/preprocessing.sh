57807d76729b64cbe43e817b2fd926564bdb6785
davidebolo1993 <davidebolognini7@gmail.com>
2020-07-07 12:54:03 +0200

    commit

--- scripts/preprocessing.sh
+++ scripts/preprocessing.sh
@@ -45,11 +45,14 @@ while getopts "hd:r:g:t:o:l:" opt; do
 done
 
 shift $((OPTIND-1))
+
 mkdir ${o}
 
-### Nanopolish ###
+### NANOPOLISH ###
 
 echo "Running nanopolish pipeline to call base modifications"
+
+mkdir ${o}/minimap2
 
 #cat all the fastq in the ${r} directory
 now=$(date +"%T")
@@ -62,7 +65,7 @@ now=$(date +"%T")
 
 echo "[${now}] [cat] Done"
 
-#create nanopolish index. The default sequencing summary does not seem to work to speed up indexing, the one from Albacore is probably different
+#create nanopolish index. The default sequencing summary does not seem to work to speed up indexing, the one from albacore is probably different
 
 echo "[${now}] [nanopolish index]"
 
@@ -72,16 +75,46 @@ now=$(date +"%T")
 
 echo "[${now}] [nanopolish index] Done"
 
-#align with minimap2. Running with the default parameter settings and --MD tag enabled for further variant calling??
+#align with minimap2. Running with the default parameter settings and --MD tag enabled for users who want to perform further variant calling (?)
 
-mkdir ${o}/minimap2
-
-now=$(date +"%T")
-
-echo "[${now}] [minimap2]"
+echo "[${now}] [minimap2 -x map-ont]"
 
 docker run -v $(dirname ${g})/:/genome/ -v ${r}/:/$(basename ${r})/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill minimap2 -ax map-ont -o /$(basename ${o})/minimap2/${l}.sam --MD -t ${t} /genome/$(basename ${g}) /$(basename ${r})/${l}.fastq
 
 now=$(date +"%T")
 
-echo "[${now}] [minimap2] Done"
+echo "[${now}] [minimap2 -x map-ont] Done"
+
+#convert sam to bam
+
+echo "[${now}] [samtools view]"
+
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools view -o /$(basename ${o})/minimap2/${l}.bam -@ ${t} /$(basename ${o})/minimap2/${l}.sam
+
+rm ${o}/minimap2/${l}.sam
+
+now=$(date +"%T")
+
+echo "[${now}] [samtools view] Done"
+
+#sort bam
+
+echo "[${now}] [samtools sort]"
+
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools sort -o /$(basename ${o})/minimap2/${l}.srt.bam -@ ${t} /$(basename ${o})/minimap2/${l}.bam
+
+rm ${o}/minimap2/${l}.bam
+
+now=$(date +"%T")
+
+echo "[${now}] [samtools sort] Done"
+
+#index
+
+echo "[${now}] [samtools index]"
+
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools index -@ ${t} /$(basename ${o})/minimap2/${l}.srt.bam
+
+now=$(date +"%T")
+
+echo "[${now}] [samtools index] Done"
