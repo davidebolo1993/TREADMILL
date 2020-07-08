873309fe450d5ab5de1b684939d9a8aaf8cd7027
davidebolo1993 <davidebolognini7@gmai.com>
2020-07-08 11:03:25 +0200

    committing

--- scripts/preprocessing.sh
+++ scripts/preprocessing.sh
@@ -2,9 +2,9 @@
 
 #parse command line options
 
-usage() { echo "Usage: $0 [-d <fast5.dir>] [-r <fastq.dir>] [-g <genome.fa>] [-t <threads.int>] [-o <output.dir>] [-l <label>]" 1>&2; exit 1;}
+usage() { echo "Usage: $0 [-d <fast5.dir>] [-r <fastq.dir>] [-g <genome.fa>] [-b <regions.optional.bed>] [-t <threads.int>] [-o <output.dir>] [-l <label>]" 1>&2; exit 1;}
 
-while getopts "hd:r:g:t:o:l:" opt; do
+while getopts "hd:r:g:b:t:o:l:" opt; do
     case "${opt}" in
 
         d)
@@ -18,6 +18,10 @@ while getopts "hd:r:g:t:o:l:" opt; do
         g)
             g=$(readlink -f ${OPTARG})
             ;;
+
+	b)
+	    b=$(readlink -f ${OPTARG})
+	    ;;
 
         t)
             t=${OPTARG}
@@ -70,7 +74,7 @@ echo "[${now}] [cat] Done"
 
 echo "[${now}] [nanopolish index]"
 
-docker run -v ${r}/:/$(basename ${r})/ -v ${d}/:/$(basename ${d})/ -ti davidebolo1993/treadmill nanopolish index -v -d /$(basename ${d})/ /$(basename ${r})/${l}.fastq
+docker run -v ${r}/:/$(basename ${r})/ -v ${d}/:/$(basename ${d})/ davidebolo1993/treadmill nanopolish index -v -d /$(basename ${d})/ /$(basename ${r})/${l}.fastq
 
 now=$(date +"%T")
 
@@ -80,7 +84,7 @@ echo "[${now}] [nanopolish index] Done"
 
 echo "[${now}] [minimap2 -x map-ont]"
 
-docker run -v $(dirname ${g})/:/genome/ -v ${r}/:/$(basename ${r})/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill minimap2 -ax map-ont -o /$(basename ${o})/minimap2/${l}.sam --MD -t ${t} /genome/$(basename ${g}) /$(basename ${r})/${l}.fastq
+docker run -v $(dirname ${g})/:/genome/ -v ${r}/:/$(basename ${r})/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ davidebolo1993/treadmill minimap2 -ax map-ont -o /$(basename ${o})/minimap2/${l}.sam --MD -t ${t} /genome/$(basename ${g}) /$(basename ${r})/${l}.fastq
 
 now=$(date +"%T")
 
@@ -90,8 +94,7 @@ echo "[${now}] [minimap2 -x map-ont] Done"
 
 echo "[${now}] [samtools view]"
 
-docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools view -o /$(basename ${o})/minimap2/${l}.bam -@ ${t} /$(basename ${o})/minimap2/${l}.sam
-
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ davidebolo1993/treadmill samtools view -o /$(basename ${o})/minimap2/${l}.bam -@ ${t} /$(basename ${o})/minimap2/${l}.sam
 rm ${o}/minimap2/${l}.sam
 
 now=$(date +"%T")
@@ -102,8 +105,7 @@ echo "[${now}] [samtools view] Done"
 
 echo "[${now}] [samtools sort]"
 
-docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools sort -o /$(basename ${o})/minimap2/${l}.srt.bam -@ ${t} /$(basename ${o})/minimap2/${l}.bam
-
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ davidebolo1993/treadmill samtools sort -o /$(basename ${o})/minimap2/${l}.srt.bam -@ ${t} /$(basename ${o})/minimap2/${l}.bam
 rm ${o}/minimap2/${l}.bam
 
 now=$(date +"%T")
@@ -114,17 +116,31 @@ echo "[${now}] [samtools sort] Done"
 
 echo "[${now}] [samtools index]"
 
-docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -ti davidebolo1993/treadmill samtools index -@ ${t} /$(basename ${o})/minimap2/${l}.srt.bam
+docker run -v ${o}/minimap2/:/$(basename ${o})/minimap2/ davidebolo1993/treadmill samtools index -@ ${t} /$(basename ${o})/minimap2/${l}.srt.bam
 
 now=$(date +"%T")
 
 echo "[${now}] [samtools index] Done"
 
-#call methylation
+#call methylation genome-wide or in regions
 
 echo "[${now}] [nanopolish call-methylation]"
 
-docker run -v $(dirname ${g})/:/genome/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -v ${o}/nanopolish/:/$(basename ${o})/nanopolish/ -v ${d}/:/$(basename ${d})/ -v ${r}/:/$(basename ${r})/ -ti davidebolo1993/treadmill nanopolish call-methylation -v --progress -r /$(basename ${r})/${l}.fastq -b /$(basename ${o})/minimap2/${l}.srt.bam -g /genome/$(basename ${g}) -q cpg -t ${t} > ${o}/nanopolish/${l}.methylation_calls.tsv
+if [ -f "${b}" ]
+
+then
+
+	while IFS=$'\t' read -r chrom start end; do
+
+        	docker run -v $(dirname ${g})/:/genome/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -v ${o}/nanopolish/:/$(basename ${o})/nanopolish/ -v ${d}/:/$(basename ${d})/ -v ${r}/:/$(basename ${r})/ davidebolo1993/treadmill nanopolish call-methylation -v --progress -r /$(basename ${r})/${l}.fastq -b /$(basename ${o})/minimap2/${l}.srt.bam -g /genome/$(basename ${g}) -q cpg -t ${t} -w "${chrom}":"$(printf "%'d" ${start})"-"$(printf "%'d" ${end})" > ${o}/nanopolish/${l}.${chrom}":"${start}"-"${end}.methylation_calls.tsv 2> /dev/null
+
+	done < ${b}
+
+else
+
+	docker run -v $(dirname ${g})/:/genome/ -v ${o}/minimap2/:/$(basename ${o})/minimap2/ -v ${o}/nanopolish/:/$(basename ${o})/nanopolish/ -v ${d}/:/$(basename ${d})/ -v ${r}/:/$(basename ${r})/ davidebolo1993/treadmill nanopolish call-methylation -v --progress -r /$(basename ${r})/${l}.fastq -b /$(basename ${o})/minimap2/${l}.srt.bam -g /genome/$(basename ${g}) -q cpg -t ${t} > ${o}/nanopolish/${l}.methylation_calls.tsv 2> /dev/null
+
+fi
 
 now=$(date +"%T")
 
